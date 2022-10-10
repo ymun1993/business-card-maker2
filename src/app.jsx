89237aa07161d3c5737cf4d551f9dceb0e1c1145
ymun1993 <ymun76@gmail.com>
2022-10-10 12:55:47 -0400

    add title UI

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,22 @@
-import './app.module.css';
-import Login from './components/login/login';
-import styles from './app.module.css';
+import "./app.module.css";
+import Login from "./components/login/login";
+import styles from "./app.module.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Maker from "./components/maker/maker";
 
 function App({ authService }) {
   return (
     <div className={styles.app}>
-      <Login authService={authService} />;
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Login authService={authService} />
+          </Route>
+          <Route path="/maker">
+            <Maker authService={authService} />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
